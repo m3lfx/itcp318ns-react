42be7ec961aef605bdd66b7526061eb573fab3a1
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-26 08:59:31 +0800

    get token

--- src/Create.js
+++ src/Create.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Title from './Title';
 import axios from 'axios';
 import Nav from './Nav';
+import { getToken } from './helpers';
 
 const Create = () => {
     const [state, setState] = useState({
@@ -22,14 +23,19 @@ const Create = () => {
 
     const handleSubmit = event => {
         event.preventDefault();
+        const config = {
+            headers: {
+                authorization: `Bearer ${getToken()}`
+            }
+        }
         // console.table({ title, content, user });
-        axios.post(`${process.env.REACT_APP_API}/posts`, { title, content, user }).then(response => {
-                console.log(response);
-                setState({ ...state, title: '', content: '', user: '' });
-                // show sucess alert
-                alert(`Post titled ${response.data.data.title} is created`);
-                return navigate("/");
-            })
+        axios.post(`${process.env.REACT_APP_API}/posts`, { title, content, user }, config).then(response => {
+            console.log(response);
+            setState({ ...state, title: '', content: '', user: '' });
+            // show sucess alert
+            alert(`Post titled ${response.data.data.title} is created`);
+            return navigate("/");
+        })
             .catch(error => {
                 console.log(error.response);
                 alert(error.response.data.error);
@@ -37,48 +43,48 @@ const Create = () => {
     };
     return (
         <>
-        <Nav />
-        <div className="container p-5">
-            <Title title="create a new post" user="wick" />
-            <br />
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label className="text-muted">Title</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Post title"
-                        required
-                        onChange={handleChange('title')}
-                        value={title}
-                    />
+            <Nav />
+            <div className="container p-5">
+                <Title title="create a new post" user="wick" />
+                <br />
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label className="text-muted">Title</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Post title"
+                            required
+                            onChange={handleChange('title')}
+                            value={title}
+                        />
 
-                </div>
-                <div className="form-group">
-                    <label className="text-muted">Content</label>
-                    <textarea
-                        type="text"
-                        className="form-control"
-                        placeholder="Write something.."
-                        required
-                        onChange={handleChange('content')}
-                        value={content} />
-                </div>
-                <div className="form-group">
-                    <label className="text-muted">User</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Your name"
-                        required
-                        onChange={handleChange('user')}
-                        value={user} />
-                </div>
-                <div>
-                    <button className="btn btn-primary" >Create</button>
-                </div>
-            </form>
-        </div>
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">Content</label>
+                        <textarea
+                            type="text"
+                            className="form-control"
+                            placeholder="Write something.."
+                            required
+                            onChange={handleChange('content')}
+                            value={content} />
+                    </div>
+                    <div className="form-group">
+                        <label className="text-muted">User</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Your name"
+                            required
+                            onChange={handleChange('user')}
+                            value={user} />
+                    </div>
+                    <div>
+                        <button className="btn btn-primary" >Create</button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
--- src/helpers.js
+++ src/helpers.js
@@ -6,3 +6,30 @@ export const authenticate = (response, next) => {
     }
     next();
 };
+
+export const getToken = () => {
+    if (window !== 'undefined') {
+        if (sessionStorage.getItem('token')) {
+            return JSON.parse(sessionStorage.getItem('token'));
+        } else {
+            return false;
+        }
+    }
+};
+export const getUser = () => {
+    if (window !== 'undefined') {
+        if (sessionStorage.getItem('user')) {
+            return JSON.parse(sessionStorage.getItem('user'));
+        } else {
+            return false;
+        }
+    }
+};
+// remove token from session storage
+export const logout = next => {
+    if (window !== 'undefined') {
+        sessionStorage.removeItem('token');
+        sessionStorage.removeItem('user');
+    }
+    next();
+};
